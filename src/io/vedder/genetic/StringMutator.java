91a933da041444c644e13d3a956c707da9d587a0
kylevedder <kylecvedder@gmail.com>
2016-04-21 23:05:58 -0400

    Added Utils class

--- src/io/vedder/genetic/StringMutator.java
+++ src/io/vedder/genetic/StringMutator.java
@@ -20,7 +20,8 @@ public class StringMutator implements Mutator {
 		int numSteps = 0;
 		while (!currentString.equals(goal)) {
 			String mutatedVersion = mutate(currentString);
-			if (computeLevenshteinDistance(mutatedVersion, goal) <= computeLevenshteinDistance(currentString, goal)) {
+			if (Utils.levenshteinDistance(mutatedVersion, goal) <= Utils
+					.levenshteinDistance(currentString, goal)) {
 				currentString = mutatedVersion;
 				System.out.println(currentString);
 			}
@@ -50,34 +51,6 @@ public class StringMutator implements Mutator {
 		default:// Remove random letter
 			return firstPart + secondPart;
 		}
-	}
-
-	/*
-	 * Levenshtein Distance Functions:
-	 *
-	 * Source:
-	 * 
-	 * https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/
-	 * Levenshtein_distance#Java
-	 */
-	private int minimum(int a, int b, int c) {
-		return Math.min(Math.min(a, b), c);
-	}
-
-	public int computeLevenshteinDistance(CharSequence lhs, CharSequence rhs) {
-		int[][] distance = new int[lhs.length() + 1][rhs.length() + 1];
-
-		for (int i = 0; i <= lhs.length(); i++)
-			distance[i][0] = i;
-		for (int j = 1; j <= rhs.length(); j++)
-			distance[0][j] = j;
-
-		for (int i = 1; i <= lhs.length(); i++)
-			for (int j = 1; j <= rhs.length(); j++)
-				distance[i][j] = minimum(distance[i - 1][j] + 1, distance[i][j - 1] + 1,
-						distance[i - 1][j - 1] + ((lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1));
-
-		return distance[lhs.length()][rhs.length()];
 	}
 
 }
